607aeee975a2b0c0ad16cfa54e5cad37054bba84
Simon <milkjhang@hotmail.com>
2022-06-06 00:29:39 +0800

    using Row to layout pages

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "@emotion/styled";
 import ProjectList from "screens/project-list";
+import { Row } from "components/lib";
 
 const Container = styled.div`
 display: grid;
@@ -8,35 +9,29 @@ grid-template-rows: 6rem 1fr 6rem;
 grid-template-columns: 20rem 1fr 20rem;
 grid-template-areas: 
 "header header header"
-"nav main aside"
-"footer footer footer";
+"main main main";
 height: 100vh;
 grid-gap: 1rem;
 `
-const Header = styled.header`
+const Header = styled(Row)`
 grid-area: header;
-display: flex;
 flex-direction: row;
-align-items: center;
-justify-content: space-between;
 `
-const HeaderLeft = styled.div`
-display: flex;
-align-items: center;
-`
+const HeaderLeft = styled(Row)``
 const HeaderRight = styled.div``
-const Nav = styled.nav`
-grid-area: nav;
-`
+
+// const Nav = styled.nav`
+// grid-area: nav;
+// `
 const Main = styled.main`
 grid-area: main;
 `
-const Aside = styled.aside`
-grid-area: aside;
-`
-const Footer = styled.footer`
-grid-area: footer;
-`
+// const Aside = styled.aside`
+// grid-area: aside;
+// `
+// const Footer = styled.footer`
+// grid-area: footer;
+// `
 
 // const PageHeader = styled.header`
 // 	height: 6rem;
@@ -47,22 +42,24 @@ grid-area: footer;
 // `
 
 export const AuthenticatedApp = () => {
-	return <Container>
-		<Header>
-			<HeaderLeft>
-				<h3>Logo</h3>
-				<h3>項目</h3>
-				<h3>用戶</h3>
-			</HeaderLeft>
-			<HeaderRight>
-				<button>登出</button>
-			</HeaderRight>
-		</Header>
-		<Nav>Nav</Nav>
-		<Main>
-			<ProjectList />
-		</Main>
-		<Aside>aside</Aside>
-		<Footer>Footer</Footer>
-	</Container>
+	return (
+		<Container>
+			<Header between={false}>
+				<HeaderLeft gap={true}>
+					<h3>Logo</h3>
+					<h3>項目</h3>
+					<h3>用戶</h3>
+				</HeaderLeft>
+				<HeaderRight>
+					<button>登出</button>
+				</HeaderRight>
+			</Header>
+			{/* <Nav>Nav</Nav> */}
+			<Main>
+				<ProjectList />
+			</Main>
+			{/* <Aside>aside</Aside>
+		<Footer>Footer</Footer> */}
+		</Container>
+	)
 }
